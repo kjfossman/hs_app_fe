2847c98aa3881e872bd75d9ada831244a929aa4f
Kyle <kjfossman@gmail.com>
2021-08-24 16:24:10 -0800

    Got google auth set up for the react frontend.  Need to hide the clientId but for now we will continue setting up the rest of the login and come back to that in the future.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
 import './App.css';
 import React, { Component } from 'react';
-
+import GoogleLogin from 'react-google-login'
+import REACT_APP_GOOGLE_CLIENT_ID from './.env'
+import REACT_APP_TEST from './.env'
 
 
 
 class App extends Component {
 
-  handleClick = () => {
-    console.log('test')
+
+  responseGoogle = (response) => {
+    console.log(response)
   }
 
   render(){
   return (
     <div className="App">
-      <button onClick={this.handleClick}>Login With Google</button>
+      <GoogleLogin
+        clientId='809855670413-r308laolsctmfsnpr3nn677h20rmmt5t.apps.googleusercontent.com'
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        // cookiePolicy={'single_host_origin'}
+      />
+      <button>Login With Google</button>
+      
     </div>
   );
 }
 }
 
 export default App;
+
+
+
+  // handleClick = () => {
+  //   console.log('test')
+  //   fetch("http://localhost:3000/auth/google_oauth2/callback", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+        
+  //     })
+  //   })
+  //   .then(result => result.json())
+  //   .then(responseJSON => {
+  //     debugger
+  //   })
+  // }
